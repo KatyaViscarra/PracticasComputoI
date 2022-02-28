c27ae0148c106f6b5c038500818ea66ebd05f27d
KatyaViscarra <shirohayama1509@gmail.com>
2022-02-27 23:01:32 -0600

    Resubida de practicas

---
+++ Practica5/componentsvuejs/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+      <!--list-products title="List Products"/>
+      <cart-items :cartItems="cartItems" :itemCount="itemCount"-->
+        
+      <life-cycle/>
+  </div>
+</template>
+
+<script>
+//import CartItems from './components/CartItems.vue'
+//import ListProducts from './components/ListProducts.vue'
+import LifeCycle from './components/LifeCycle.vue'
+
+export default {
+  name: 'App',
+  components: {
+    //ListProducts,
+    //CartItems
+    LifeCycle
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
